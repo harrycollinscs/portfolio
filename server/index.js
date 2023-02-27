fa19e46e8c2646b5293dbd648db04424ca4c011c
Harry Collins <harrycol97@gmail.com>
2023-02-27 20:46:36 +0000

    allows cors

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const { default: axios } = require("axios");
 
 require('dotenv').config()
 
-const { PORT, CLIENT_URL, SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, SPOTIFY_REFRESH_TOKEN, SPOTIFY_REFRESH_TOKEN_URL, SPOTIFY_RECENTLY_PLAYED_URL } = process.env
+const { PORT, SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, SPOTIFY_REFRESH_TOKEN, SPOTIFY_REFRESH_TOKEN_URL, SPOTIFY_RECENTLY_PLAYED_URL } = process.env
 
 const app = express();
 
@@ -24,14 +24,10 @@ const headers = {
   Authorization: `Basic ${auth}`,
 };
 
-const corsOptions = {
-  origin: CLIENT_URL,
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", '*');
 
   res.setHeader(
     "Access-Control-Allow-Methods",
